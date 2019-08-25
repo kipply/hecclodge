d3510122e42ca82265b99f96ff91c4871edadaa9
Carol Chen <carol.chen@shopify.com>
2019-08-25 17:05:55 -0400

    Add beat extractor

---
+++ app/beat_extractor.py
@@ -0,0 +1,11 @@
+import essentia
+import pdb
+from essentia.standard import BeatTrackerDegara
+
+
+def get_beats(filename):
+    loader = essentia.standard.MonoLoader(filename='static/' + filename)
+    audio = loader()
+
+    beat_tracker = BeatTrackerDegara()
+    return beat_tracker(audio).tolist()
