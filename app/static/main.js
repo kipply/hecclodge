e28c45c90dcaafa1032ceae94858ed7d9b449f58
Nicholas Lai <nicholaslai@Cero.local>
2019-08-25 23:39:23 -0400

    Put placeholder images and also added a score which follows player

--- app/static/main.js
+++ app/static/main.js
@@ -25,16 +25,19 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var score = 0;
+var scoreText;
 
 function preload ()
 {
     // Load in images and sprites
 
-    this.load.spritesheet('player_handgun', 'assets/player_handgun.png',
+    this.load.spritesheet('player_handgun', 'static/player_handgun.png',
         { frameWidth: 66, frameHeight: 60 }
     ); // Made by tokkatrain: https://tokkatrain.itch.io/top-down-basic-set
-    this.load.image('target', 'assets/ball.png');
-    this.load.image('background', 'assets/underwater1.png');
+    this.load.image('target', 'static/ball.png');
+    this.load.image('background', 'static/starfield.png');
+    this.load.image('bullet', 'static/bullet72.png');
 }
 
 function create ()
@@ -47,16 +50,19 @@ function create ()
     player = this.physics.add.sprite(800, 600, 'player_handgun');
     reticle = this.physics.add.sprite(800, 700, 'target');
 
+    //add score text
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#FFFFFF'});
+
     //add bullets flying around
     for (var i = 0; i < 30; i++) {
-        var bullet = this.physics.add.image(400, 100, 'logo');
+        var bullet = this.physics.add.image(400, 100, 'bullet');
         bullet.setVelocity(Math.random() * 100, Math.random() * 100);
         bullet.setBounce(1, 1);
         bullet.setCollideWorldBounds(true);
         //this.physics.add.collider(player, bullet);
         this.physics.add.overlap(player, bullet, destroyBullet, null, this);
     }
-
+    
     // Set image/sprite properties
     background.setOrigin(0.5, 0.5).setDisplaySize(1600, 1200);
     player.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setCollideWorldBounds(true).setDrag(500, 500);
@@ -129,6 +135,8 @@ function create ()
 
 function destroyBullet(player, bullet) { //destroys bullet
     bullet.disableBody(true, true);
+    score += 10;
+    scoreText.setText('Score: ' + score);
 }
 
 // Ensures sprite speed doesnt exceed maxVelocity while update is called
@@ -178,6 +186,9 @@ function update (time, delta)
     // Camera follows player ( can be set in create )
     this.cameras.main.startFollow(player);
 
+    scoreText.x = player.x - 700;
+    scoreText.y = player.y - 550;
+
     // Makes reticle move with player
     reticle.body.velocity.x = player.body.velocity.x;
     reticle.body.velocity.y = player.body.velocity.y;
@@ -187,5 +198,5 @@ function update (time, delta)
 
     // Constrain position of reticle
     constrainReticle(reticle);
-
+    
 }
