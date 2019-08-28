44dbfce2fa4088b78c3dff36124be4577803ec44
Carol Chen <carol.chen@shopify.com>
2019-08-28 18:17:06 -0400

    make lib file

--- app/static/js/SceneMain.js
+++ app/static/js/SceneMain.js
@@ -328,19 +328,3 @@ var Game = new Phaser.Class({
     metronomeTicker.strokeRect(windowHeight/3 + progress*windowWidth/2, -windowHeight/2 + 50, 5, 75);
   }
 });
-
-function getRandomPoint() {
-  let point = {};
-  point['x'] = Math.floor(Math.random() * (windowWidth * 2 - 50) + (-windowWidth / 2 + 50));
-  point['y'] = Math.floor(Math.random() * (windowHeight * 2 - 50) + (-windowHeight / 2 + 50));
-  return point;
-}
-
-function getDistance(one, two) {
-  return Math.pow(Math.pow(one.x - two.x, 2) + Math.pow(one.y - two.y, 2), 0.5)
-}
-
-function getBufferArea() {
-  return Math.max(windowHeight, windowWidth) * 0.05;
-}
-
