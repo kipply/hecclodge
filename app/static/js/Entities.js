76440678f081b9f601844876b7cc9e32d8639fdc
Nicholas Lai <nicholaslai749@gmail.com>
2019-08-29 15:21:56 -0400

    Added more sample enemy waves

--- app/static/js/Entities.js
+++ app/static/js/Entities.js
@@ -45,7 +45,8 @@ class BurstEnemy extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, "sprEnemy0", "GunShip");
     this.play("sprEnemy0");
-    this.body.velocity.y = Phaser.Math.Between(50,100);
+    this.body.velocity.y = 50;
+    this.body.acceleration.y = -20;
     this.shootTimer = this.scene.time.addEvent({
       delay: 1000,
       callback: function() {
